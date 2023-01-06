d35a8a6fc8626927a6d0a13f69b0f9911670f104
yuna <yunae426@gmail.com>
2023-01-06 17:50:14 +0900

    react prac

--- DreamCoding/css/src/App.js
+++ DreamCoding/css/src/App.js
@@ -1,39 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Button1 from './components/Button1';
 import Button2 from './components/Button2';
-import styled from 'styled-components';
-import { css } from 'styled-components';
-
-const Container = styled.div`
-  display : flex;
-`;
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #3c5b69;
-  color: #b9eaff;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  ${(props)=>
-    props.primary &&
-    css`
-    background: #009cd5;
-    color: white;
-    `
-  }
-`
+import StyledComponent from './StyledComponent';
+import TailwindComponent from './TailwindComponent';
 
 function App() {
   return (
     <>
       <Button1/>
       <Button2/>
-      <Container>
-        <Button>Normal Button</Button>
-        <Button primary>Primary Button</Button>
-      </Container>
+      <StyledComponent/>
+      <TailwindComponent/>
     </>
   );
 }
---
+++ DreamCoding/css/src/TailwindComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+    return (
+        <div>
+          <h1 className='text-3xl'>안녕!</h1>
+          <button className='bg-blue-500 rounded-xl px-2'>button</button>
+        </div>
+    );
+}
+
+
