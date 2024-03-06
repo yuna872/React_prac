d1a80221ba018062c82937749bdaf04766f13867
yuna_kim <yunae426@gmail.com>
2024-03-06 09:05:22 +0900

    Section 0. Installation

--- 리액트공식문서스터디/src/main.tsx
+++ 리액트공식문서스터디/src/main.tsx
@@ -8,3 +8,16 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <LikeButton />
   </React.StrictMode>,
 )
+// 각각 재사용할 수 있음
+ReactDOM.createRoot(document.getElementById('root-2')!).render(
+  <React.StrictMode>
+    <LikeButton />
+  </React.StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('root-3')!).render(
+  <React.StrictMode>
+    <LikeButton />
+  </React.StrictMode>,
+)
+
